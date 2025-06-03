d9727992abee52f709a60c35541d155ad1c704b7
cynthiapeng910 <cynthiapeng910@gmail.com>
2025-06-03 16:35:58 +0800

    Update contact.py

--- page_content/contact.py
+++ page_content/contact.py
@@ -8,7 +8,7 @@ def contact_page():
     
     ### Direct Contact
     - **Email**: [sarah.johnson@example.com](mailto:sarah.johnson@example.com)
-    - **Phone**: +1 (123) 456-7890
+    - **Phone**: +852 46368146
     - **LinkedIn**: [linkedin.com/in/sarahjohnson](https://linkedin.com/in/sarahjohnson)
     - **GitHub**: [github.com/sarahjohnson](https://github.com/sarahjohnson)
     """)
